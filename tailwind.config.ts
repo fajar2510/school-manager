97e3875367b6a1b98a795072177591fb4e9d811f
fajar2510 <sepertiseharusnyafajar@gmail.com>
2024-12-15 18:30:49 +0700

    fist-commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        customSky: "#C3EBFA",
+        customSkyLight: "#EDF9FD",
+        customPurple: "#CFCEFF",
+        customPurpleLight: "#F1F0FF",
+        customYellow: "#FAE27C",
+        customYellowLight: "#FEFCE8",
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
+export default config;
